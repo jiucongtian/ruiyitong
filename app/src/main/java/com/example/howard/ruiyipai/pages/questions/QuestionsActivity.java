3dd1e929070cea746f6a5f46438545875a30da7d
aqua <10636059@qq.com>
2018-01-24 16:23:36 +0800

    调整播放声音代码

--- app/src/main/java/com/example/howard/ruiyipai/pages/questions/QuestionsActivity.java
+++ app/src/main/java/com/example/howard/ruiyipai/pages/questions/QuestionsActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.DragEvent;
 import android.view.View;
 
+
 import com.example.howard.ruiyipai.R;
 import com.example.howard.ruiyipai.base.BaseActivity;
 import com.example.howard.ruiyipai.pages.questions.bean.StudentSelectionBean;
@@ -60,17 +61,17 @@ public class QuestionsActivity extends BaseActivity {
     RecyclerView mStudentSelector;
 
 
-    SoundPool mSoundPool;
-
-    private HashMap<Integer, Integer> soundID = new HashMap<Integer, Integer>();
+//    SoundPool mSoundPool;
+//
+//    private HashMap<Integer, Integer> soundID = new HashMap<Integer, Integer>();
 
     @Override
     public void initPages() {
-        try {
-            initSoundPool();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            initSoundPool();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         initStudentsList();
 
         initSingleQuestionsNumber();
@@ -267,29 +268,31 @@ public class QuestionsActivity extends BaseActivity {
     }
 
 
-    private void initSoundPool() throws IOException {
-        //当前系统的SDK版本大于等于21(Android 5.0)时
-        if (Build.VERSION.SDK_INT >= 21) {
-            SoundPool.Builder builder = new SoundPool.Builder();
-            //传入音频数量
-            builder.setMaxStreams(2);
-            //AudioAttributes是一个封装音频各种属性的方法
-            AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
-            //设置音频流的合适的属性
-            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC);
-            //加载一个AudioAttributes
-            builder.setAudioAttributes(attrBuilder.build());
-            mSoundPool = builder.build();
-        }
-        //当系统的SDK版本小于21时
-        else {//设置最多可容纳2个音频流，音频的品质为5
-            mSoundPool = new SoundPool(2, AudioManager.STREAM_SYSTEM, 5);
-        }
-
-        soundID.put(1, mSoundPool.load(this, R.raw.sound, 1));
-    }
+//    private void initSoundPool() throws IOException {
+//        //当前系统的SDK版本大于等于21(Android 5.0)时
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            SoundPool.Builder builder = new SoundPool.Builder();
+//            //传入音频数量
+//            builder.setMaxStreams(2);
+//            //AudioAttributes是一个封装音频各种属性的方法
+//            AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+//            //设置音频流的合适的属性
+//            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC);
+//            //加载一个AudioAttributes
+//            builder.setAudioAttributes(attrBuilder.build());
+//            mSoundPool = builder.build();
+//        }
+//        //当系统的SDK版本小于21时
+//        else {//设置最多可容纳2个音频流，音频的品质为5
+//            mSoundPool = new SoundPool(2, AudioManager.STREAM_SYSTEM, 5);
+//        }
+//
+////        soundID.put(1, mSoundPool.load(this, R.raw.sound, 1));
+//    }
 
     protected void playSound() {
-        mSoundPool.play(soundID.get(1), 1, 1, 0, 0, 1);
+//        mSoundPool.play(soundID.get(1), 0.5f, 0.5f, 0, 0, 1);
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD,1);
     }
 }
