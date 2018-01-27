b668473977b16ec381a80a736657dfe28bcc24ff
zhenghao <jiucongtian@163.com>
2018-01-27 12:37:25 +0800

    调整barchart

--- app/src/main/java/com/example/howard/ruiyipai/pages/stastic/fragment/AnswerChartFragment.java
+++ app/src/main/java/com/example/howard/ruiyipai/pages/stastic/fragment/AnswerChartFragment.java
@@ -1,6 +1,8 @@
 package com.example.howard.ruiyipai.pages.stastic.fragment;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,9 +11,27 @@ import android.view.ViewGroup;
 import com.example.howard.ruiyipai.R;
 import com.example.howard.ruiyipai.base.BaseFragment;
 import com.example.howard.ruiyipai.common.Utils;
+import com.example.howard.ruiyipai.pages.TestActivity;
 import com.example.howard.ruiyipai.pages.lesson.adapter.LessonToolsAdapter;
 import com.example.howard.ruiyipai.pages.stastic.adapter.AnswerInfoAdapter;
 import com.example.howard.ruiyipai.recyclerAdapter.ClassAdapter;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -46,6 +66,9 @@ public class AnswerChartFragment extends BaseFragment {
     @BindView(R.id.rv_f)
     RecyclerView fRv;
 
+    @BindView(R.id.bc_answer_statistic)
+    BarChart answerBarChart;
+
     FragmentManager fm;
     public AnswerChartFragment(FragmentManager fragmentManager ) {
         fm = fragmentManager;
@@ -54,7 +77,7 @@ public class AnswerChartFragment extends BaseFragment {
     @Override
     protected void initPages() {
 
-
+        initBarChart();
         initRecyclerView(unanswerRv);
         initRecyclerView(aRv);
         initRecyclerView(bRv);
@@ -83,5 +106,294 @@ public class AnswerChartFragment extends BaseFragment {
     @Override
     public int getLayoutId() {
         return R.layout.fragment_answer_chart;
+    }
+
+
+    private void initBarChart() {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1, 2));
+        entries.add(new BarEntry(2, 20));
+        entries.add(new BarEntry(3, 10));
+        entries.add(new BarEntry(4, 5));
+        entries.add(new BarEntry(5, 40));
+        entries.add(new BarEntry(6, 4));
+        entries.add(new BarEntry(7, 9));
+        // 每一个BarDataSet代表一类柱状图
+        BarDataSet barDataSet = new BarDataSet(entries, "");
+
+
+        List<Integer> colors = new ArrayList<>();
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_red));
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_blue));
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_blue));
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_blue));
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_blue));
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_blue));
+        colors.add(ContextCompat.getColor(getContext(), R.color.bar_chart_blue));
+
+        barDataSet.setColors(colors);
+
+
+        barDataSet.setValueTextSize(9f);
+        barDataSet.setFormLineWidth(1f);
+        barDataSet.setFormSize(15.f);
+        barDataSet.setValueFormatter(new IValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                return String.valueOf((int)value);
+            }
+        });
+
+
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(barDataSet);
+        BarData data = new BarData(dataSets);
+        //设置X轴的刻度数
+        answerBarChart.setData(data);
+
+
+        //获取x轴
+        XAxis xAxis = answerBarChart.getXAxis();
+        xAxis.setDrawGridLines(false);
+
+        xAxis.setAxisMaximum(8);
+        xAxis.setAxisMinimum(0);
+        xAxis.setLabelCount(9, false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f);
+        xAxis.setDrawLabels(false);
+
+
+        //获取y轴
+        YAxis yAxis = answerBarChart.getAxisLeft();
+        yAxis.setDrawGridLines(false);
+        yAxis.setLabelCount(5, true);
+        yAxis.setAxisMinimum(0);
+        yAxis.setAxisMaximum(40);
+        yAxis.setSpaceTop(20f);
+
+
+        Legend legend = answerBarChart.getLegend();             //左下角比例尺
+        legend.setEnabled(false);
+        answerBarChart.getAxisRight().setEnabled(false);        //右侧坐标轴
+        answerBarChart.setDrawBorders(false);                   //隐藏最外层方框
+        answerBarChart.setTouchEnabled(false);
+        answerBarChart.setDrawValueAboveBar(true);
+        answerBarChart.animateY(1000, Easing.EasingOption.Linear);
+
+
+        Description desc = new Description();
+        desc.setText("");
+
+        answerBarChart.setDescription(desc);
+    }
+
+
+    public class BarChartManager {
+        private BarChart mBarChart;
+        private YAxis leftAxis;
+        private YAxis rightAxis;
+        private XAxis xAxis;
+
+        public BarChartManager(BarChart barChart) {
+            this.mBarChart = barChart;
+            leftAxis = mBarChart.getAxisLeft();
+            rightAxis = mBarChart.getAxisRight();
+            xAxis = mBarChart.getXAxis();
+        }
+
+        /**
+         * 初始化LineChart
+         */
+        private void initLineChart() {
+            //背景颜色
+            mBarChart.setBackgroundColor(Color.WHITE);
+            //网格
+            mBarChart.setDrawGridBackground(false);
+            //背景阴影
+            mBarChart.setDrawBarShadow(false);
+            mBarChart.setHighlightFullBarEnabled(false);
+
+            //显示边界
+            mBarChart.setDrawBorders(true);
+            //设置动画效果
+            mBarChart.animateY(1000, Easing.EasingOption.Linear);
+            mBarChart.animateX(1000, Easing.EasingOption.Linear);
+
+            //折线图例 标签 设置
+            Legend legend = mBarChart.getLegend();
+            legend.setForm(Legend.LegendForm.LINE);
+            legend.setTextSize(11f);
+            //显示位置
+            legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+            legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+            legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+            legend.setDrawInside(false);
+
+            //XY轴的设置
+            //X轴设置显示位置在底部
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setGranularity(1f);
+            //保证Y轴从0开始，不然会上移一点
+            leftAxis.setAxisMinimum(0f);
+            rightAxis.setAxisMinimum(0f);
+        }
+
+        /**
+         * 展示柱状图(一条)
+         *
+         * @param xAxisValues
+         * @param yAxisValues
+         * @param label
+         * @param color
+         */
+        public void showBarChart(List<Float> xAxisValues, List<Float> yAxisValues, String label, int color) {
+            initLineChart();
+            ArrayList<BarEntry> entries = new ArrayList<>();
+            for (int i = 0; i < xAxisValues.size(); i++) {
+                entries.add(new BarEntry(xAxisValues.get(i), yAxisValues.get(i)));
+            }
+            // 每一个BarDataSet代表一类柱状图
+            BarDataSet barDataSet = new BarDataSet(entries, label);
+
+            barDataSet.setColor(color);
+            barDataSet.setValueTextSize(9f);
+            barDataSet.setFormLineWidth(1f);
+            barDataSet.setFormSize(15.f);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(barDataSet);
+            BarData data = new BarData(dataSets);
+            //设置X轴的刻度数
+            xAxis.setLabelCount(xAxisValues.size() - 1, false);
+            mBarChart.setData(data);
+        }
+
+        /**
+         * 展示柱状图(多条)
+         *
+         * @param xAxisValues
+         * @param yAxisValues
+         * @param labels
+         * @param colours
+         */
+        public void showBarChart(List<Float> xAxisValues, List<List<Float>> yAxisValues, List<String> labels, List<Integer> colours) {
+            initLineChart();
+            BarData data = new BarData();
+            for (int i = 0; i < yAxisValues.size(); i++) {
+                ArrayList<BarEntry> entries = new ArrayList<>();
+                for (int j = 0; j < yAxisValues.get(i).size(); j++) {
+
+                    entries.add(new BarEntry(xAxisValues.get(j), yAxisValues.get(i).get(j), j));
+                }
+                BarDataSet barDataSet = new BarDataSet(entries, labels.get(i));
+
+                barDataSet.setColor(colours.get(i));
+                barDataSet.setValueTextColor(colours.get(i));
+                barDataSet.setValueTextSize(10f);
+                barDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                data.addDataSet(barDataSet);
+            }
+            int amount = yAxisValues.size();
+
+            float groupSpace = 0.12f; //柱状图组之间的间距
+            float barSpace = (float) ((1 - 0.12) / amount / 10); // x4 DataSet
+            float barWidth = (float) ((1 - 0.12) / amount / 10 * 9); // x4 DataSet
+
+            // (0.2 + 0.02) * 4 + 0.08 = 1.00 -> interval per "group"
+            xAxis.setLabelCount(xAxisValues.size() - 1, false);
+            data.setBarWidth(barWidth);
+
+
+            data.groupBars((float) 0.5, groupSpace, barSpace);
+            mBarChart.setData(data);
+        }
+
+
+        /**
+         * 设置Y轴值
+         *
+         * @param max
+         * @param min
+         * @param labelCount
+         */
+        public void setYAxis(float max, float min, int labelCount) {
+            if (max < min) {
+                return;
+            }
+            leftAxis.setAxisMaximum(max);
+            leftAxis.setAxisMinimum(min);
+            leftAxis.setLabelCount(labelCount, false);
+
+            rightAxis.setAxisMaximum(max);
+            rightAxis.setAxisMinimum(min);
+            rightAxis.setLabelCount(labelCount, false);
+            mBarChart.invalidate();
+        }
+
+        /**
+         * 设置X轴的值
+         *
+         * @param max
+         * @param min
+         * @param labelCount
+         */
+        public void setXAxis(float max, float min, int labelCount) {
+            xAxis.setAxisMaximum(max);
+            xAxis.setAxisMinimum(min);
+            xAxis.setLabelCount(labelCount, false);
+
+            mBarChart.invalidate();
+        }
+
+        /**
+         * 设置高限制线
+         *
+         * @param high
+         * @param name
+         */
+        public void setHightLimitLine(float high, String name, int color) {
+            if (name == null) {
+                name = "高限制线";
+            }
+            LimitLine hightLimit = new LimitLine(high, name);
+            hightLimit.setLineWidth(4f);
+            hightLimit.setTextSize(10f);
+            hightLimit.setLineColor(color);
+            hightLimit.setTextColor(color);
+            leftAxis.addLimitLine(hightLimit);
+            mBarChart.invalidate();
+        }
+
+        /**
+         * 设置低限制线
+         *
+         * @param low
+         * @param name
+         */
+        public void setLowLimitLine(int low, String name) {
+            if (name == null) {
+                name = "低限制线";
+            }
+            LimitLine hightLimit = new LimitLine(low, name);
+            hightLimit.setLineWidth(4f);
+            hightLimit.setTextSize(10f);
+            leftAxis.addLimitLine(hightLimit);
+            mBarChart.invalidate();
+        }
+
+        /**
+         * 设置描述信息
+         *
+         * @param str
+         */
+        public void setDescription(String str) {
+            Description description = new Description();
+            description.setText(str);
+            mBarChart.setDescription(description);
+            mBarChart.invalidate();
+        }
     }
 }
--- app/src/main/java/com/example/howard/ruiyipai/pages/stastic/fragment/AnswerDurationFragment.java
+++ app/src/main/java/com/example/howard/ruiyipai/pages/stastic/fragment/AnswerDurationFragment.java
@@ -1,6 +1,7 @@
 package com.example.howard.ruiyipai.pages.stastic.fragment;
 
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +11,22 @@ import com.example.howard.ruiyipai.R;
 import com.example.howard.ruiyipai.base.BaseFragment;
 import com.example.howard.ruiyipai.common.Utils;
 import com.example.howard.ruiyipai.pages.stastic.adapter.AnswerInfoAdapter;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -34,7 +51,8 @@ public class AnswerDurationFragment extends BaseFragment {
     @BindView(R.id.rv_below_average)
     RecyclerView belowAverageRv;
 
-
+    @BindView(R.id.lineChart)
+    LineChart mAnswerDuration;
 
     FragmentManager fm;
     public AnswerDurationFragment(FragmentManager fragmentManager ) {
@@ -44,10 +62,11 @@ public class AnswerDurationFragment extends BaseFragment {
     @Override
     protected void initPages() {
 
-    initRecyclerView(unanswerRv);
-    initRecyclerView(first3Rv);
-    initRecyclerView(aboveAverageRv);
-    initRecyclerView(belowAverageRv);
+        initLineChart();
+        initRecyclerView(unanswerRv);
+        initRecyclerView(first3Rv);
+        initRecyclerView(aboveAverageRv);
+        initRecyclerView(belowAverageRv);
     }
 
     @Override
@@ -69,5 +88,139 @@ public class AnswerDurationFragment extends BaseFragment {
         view.setLayoutManager(layoutManager);
         AnswerInfoAdapter adapter = new AnswerInfoAdapter();
         view.setAdapter(adapter);
+    }
+
+    private void initLineChart() {
+        //1part
+        //设置数据
+        List<Entry> entries = new ArrayList<>();
+
+        entries.add(new Entry(0, (float) 0));
+        entries.add(new Entry(1, (float) 0));
+        entries.add(new Entry(2, (float) 0));
+        entries.add(new Entry(3, (float) 200));
+
+        //一个LineDataSet就是一条线
+        LineDataSet lineDataSet = new LineDataSet(entries, "温度");
+        lineDataSet.setColor(ContextCompat.getColor(getContext(), R.color.line_chart_point_red));
+        lineDataSet.setCircleColor(ContextCompat.getColor(getContext(), R.color.line_chart_point_red));
+        lineDataSet.setCircleRadius(4);
+        lineDataSet.setDrawValues(false);//隐藏折线图每个数据点的值
+        lineDataSet.setDrawCircles(true);//图表上的数据点是否用小圆圈表示
+        lineDataSet.setDrawFilled(true);//是否填充折线下方
+        lineDataSet.setFillColor(ContextCompat.getColor(getContext(), R.color.line_chart_fill_red));//折线图下方填充颜色设置
+        //设置线和点结束
+
+        //2part
+        List<Entry> entries2 = new ArrayList<>();
+        entries2.add(new Entry(3, (float) 200));
+        entries2.add(new Entry(4, (float) 500));
+        entries2.add(new Entry(5, (float) 600));
+        entries2.add(new Entry(6, (float) 600));
+        entries2.add(new Entry(7, (float) 750));
+        entries2.add(new Entry(8, (float) 900));
+        entries2.add(new Entry(9, (float) 900));
+        entries2.add(new Entry(10, (float) 930));
+        entries2.add(new Entry(11, (float) 950));
+        entries2.add(new Entry(12, (float) 1000));
+        LineDataSet lineDataSet2 = new LineDataSet(entries2, "温度");
+        lineDataSet2.setColor(ContextCompat.getColor(getContext(), R.color.line_chart_point_blue));
+        lineDataSet2.setCircleColor(ContextCompat.getColor(getContext(), R.color.line_chart_point_blue));
+        lineDataSet2.setCircleRadius(4);
+        lineDataSet2.setDrawValues(false);//隐藏折线图每个数据点的值
+        lineDataSet2.setDrawCircles(true);//图表上的数据点是否用小圆圈表示
+        lineDataSet2.setDrawFilled(true);//是否填充折线下方
+        lineDataSet2.setFillColor(ContextCompat.getColor(getContext(), R.color.line_chart_fill_blue));//折线图下方填充颜色设置
+
+        //3part
+        List<Entry> entries3 = new ArrayList<>();
+        entries3.add(new Entry(12, (float) 1000));
+        entries3.add(new Entry(13, (float) 1100));
+        entries3.add(new Entry(14, (float) 1150));
+        entries3.add(new Entry(15, (float) 1600));
+        entries3.add(new Entry(16, (float) 1700));
+        entries3.add(new Entry(17, (float) 1750));
+        entries3.add(new Entry(18, (float) 1800));
+        entries3.add(new Entry(19, (float) 1900));
+        LineDataSet lineDataSet3 = new LineDataSet(entries3, "温度");
+        lineDataSet3.setColor(ContextCompat.getColor(getContext(), R.color.line_chart_point_yellow));
+        lineDataSet3.setCircleColor(ContextCompat.getColor(getContext(), R.color.line_chart_point_yellow));
+        lineDataSet3.setCircleRadius(4);
+        lineDataSet3.setDrawValues(false);//隐藏折线图每个数据点的值
+        lineDataSet3.setDrawCircles(true);//图表上的数据点是否用小圆圈表示
+        lineDataSet3.setDrawFilled(true);//是否填充折线下方
+        lineDataSet3.setFillColor(ContextCompat.getColor(getContext(), R.color.line_chart_fill_yellow));//折线图下方填充颜色设置
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet); // add the datasets
+        dataSets.add(lineDataSet2);
+        dataSets.add(lineDataSet3);
+        LineData data = new LineData(dataSets);
+        mAnswerDuration.setData(data);
+
+        XAxis xAxis = mAnswerDuration.getXAxis();
+        xAxis.setDrawAxisLine(true);                        //x坐标轴线
+        xAxis.setDrawGridLines(true);                      //x坐标轴竖线
+        xAxis.setGridColor(R.color.line_chart_grid_line);
+        xAxis.setLabelRotationAngle(-60f);                  //x轴旋转角度
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置x坐标轴显示在下边
+        xAxis.setTextSize(10);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return "学生" + String.valueOf((int)value);
+            }
+        });
+
+        //设置x轴个数
+        Class<? extends AxisBase > clazz = AxisBase.class;
+
+        try {
+            Field nameField = clazz.getDeclaredField("mLabelCount");
+            nameField.setAccessible(true);// 设置操作权限为true
+            nameField.set(xAxis, 20);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        YAxis yAxis = mAnswerDuration.getAxisLeft();
+        yAxis.setDrawAxisLine(true);     //y坐标轴线
+        yAxis.setDrawGridLines(false);   //y坐标轴竖线
+
+        //可以设置一条警戒线，如下：
+        LimitLine ll = new LimitLine(1000f, "");
+        ll.setLineColor(ContextCompat.getColor(getContext(), R.color.icon_blue_sky));
+        ll.setLineWidth(1f);
+        yAxis.addLimitLine(ll);
+        yAxis.setLabelCount(5, true);
+        yAxis.setAxisMaximum(2000);
+        yAxis.setAxisMinimum(0);
+        yAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                if (value == 0){
+                    return "00:00";
+                } else if (value == 500) {
+                    return "00:30";
+                } else if (value == 1000) {
+                    return "01:00";
+                } else if (value == 1500) {
+                    return "01:30";
+                } else if (value == 2000) {
+                    return "02:00";
+                } else {
+                    return "00:00";
+                }
+            }
+        });
+
+        Legend legend = mAnswerDuration.getLegend();             //左下角比例尺
+        legend.setEnabled(false);
+        mAnswerDuration.getAxisRight().setEnabled(false);        //右侧坐标轴
+        mAnswerDuration.setDrawBorders(false);                   //隐藏最外层方框
+        Description desc = new Description();
+        desc.setText("");
+
+        mAnswerDuration.setDescription(desc);
     }
 }
