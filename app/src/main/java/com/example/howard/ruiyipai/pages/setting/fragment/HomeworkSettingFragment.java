2ce488f30ee01467b045a33538ef611c6cc09aac
zhenghao <jiucongtian@163.com>
2018-02-07 21:41:32 +0800

    作业批改设置。

--- app/src/main/java/com/example/howard/ruiyipai/pages/setting/fragment/HomeworkSettingFragment.java
+++ app/src/main/java/com/example/howard/ruiyipai/pages/setting/fragment/HomeworkSettingFragment.java
@@ -27,16 +27,6 @@ public class HomeworkSettingFragment extends BaseFragment {
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
                 int seekProgress = seekBar.getProgress();
                 if (seekProgress < 5.6) {
                     seekBar.setProgress(0);
@@ -59,6 +49,37 @@ public class HomeworkSettingFragment extends BaseFragment {
                 } else {
                     seekBar.setProgress(100);
                 }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+//                int seekProgress = seekBar.getProgress();
+//                if (seekProgress < 5.6) {
+//                    seekBar.setProgress(0);
+//                } else if (5.6 <= seekProgress && seekProgress < 16.7) {
+//                    seekBar.setProgress(11);
+//                } else if (16.7 <= seekProgress && seekProgress < 27.8) {
+//                    seekBar.setProgress(22);
+//                } else if (27.8 <= seekProgress && seekProgress < 38.9) {
+//                    seekBar.setProgress(33);
+//                } else if (38.9 <= seekProgress && seekProgress < 50) {
+//                    seekBar.setProgress(44);
+//                } else if (50 <= seekProgress && seekProgress < 61.1) {
+//                    seekBar.setProgress(56);
+//                } else if (61.1 <= seekProgress && seekProgress < 72.2) {
+//                    seekBar.setProgress(67);
+//                } else if (72.2 <= seekProgress && seekProgress < 83.3) {
+//                    seekBar.setProgress(78);
+//                } else if (83.3 <= seekProgress && seekProgress < 94.4) {
+//                    seekBar.setProgress(89);
+//                } else {
+//                    seekBar.setProgress(100);
+//                }
             }
         });
     }
