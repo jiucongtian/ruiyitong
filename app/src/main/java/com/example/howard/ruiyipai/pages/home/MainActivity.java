cfd21412d7ceb76be02fdb346d60ad9298298b82
zhenghao <jiucongtian@163.com>
2018-02-10 16:36:14 +0800

    更按钮点击效果。

--- app/src/main/java/com/example/howard/ruiyipai/pages/home/MainActivity.java
+++ app/src/main/java/com/example/howard/ruiyipai/pages/home/MainActivity.java
@@ -256,6 +256,7 @@ public class MainActivity extends BaseActivity implements ClassAdapter.OnItemCli
         AlertDialog dlg = new AlertDialog.Builder(this, R.style.AlertDialogStyle).create();
         dlg.show();
         dlg.getWindow().setContentView(R.layout.dialog_check_version);
+        dlg.getWindow().findViewById(R.id.btn2).setSelected(true);
     }
 
     private void showNoDataDialog() {
@@ -269,7 +270,7 @@ public class MainActivity extends BaseActivity implements ClassAdapter.OnItemCli
         dlg.show();
         dlg.getWindow().setContentView(R.layout.dialog_create_folder);
 
-
+        dlg.getWindow().findViewById(R.id.btn2).setSelected(true);
 
 
         dlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
