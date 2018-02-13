91d61540275b19f3c4efaf3cee07766f5c8548bf
zhenghao <jiucongtian@163.com>
2018-02-13 18:10:04 +0800

    修改悬浮按钮

--- baselibrary/src/main/java/com/example/baselibrary/common/FloatingButton.java
+++ baselibrary/src/main/java/com/example/baselibrary/common/FloatingButton.java
@@ -3,8 +3,10 @@ package com.example.baselibrary.common;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.RelativeLayout;
 
 
@@ -17,13 +19,18 @@ import com.example.baselibrary.R;
 
 public class FloatingButton extends RelativeLayout implements View.OnClickListener {
 
+    final static int BASE_WIDTH = 60;
+    final static int WIDTH_OF_ITEM = 60;
 
-//    @BindView(R.id.ll_pop_ground)
-    View popLayout;
+    ViewGroup popLayout;
 
     Context context;
 
     boolean openStatus = false;
+
+    private int mChindCount = 0;
+
+    private int width = 0;
 
     public FloatingButton(Context context) {
         this(context, null);
@@ -48,12 +55,14 @@ public class FloatingButton extends RelativeLayout implements View.OnClickListen
         View view = View.inflate(context, R.layout.widget_floating_button, this);
 
         popLayout = view.findViewById(R.id.ll_pop_ground);
+        mChindCount = popLayout.getChildCount();
+        width = Utils.dip2px(context, BASE_WIDTH + mChindCount * WIDTH_OF_ITEM);
+        RelativeLayout.LayoutParams lp = (LayoutParams) popLayout.getLayoutParams();
+        lp.width = width;
+        lp.rightMargin = -1 * width + 1; //add 1 to avoid dismiss
+        popLayout.setLayoutParams(lp);
+
         view.findViewById(R.id.iv_camera).setOnClickListener(this);
-
-
-//        ButterKnife.bind(this, view);
-
-
     }
 
     @Override
@@ -62,12 +71,20 @@ public class FloatingButton extends RelativeLayout implements View.OnClickListen
 
         if (id == R.id.iv_camera) {
             if (openStatus) {
-                Animation anim = AnimationUtils.loadAnimation(context, R.anim.camera_reverse);
+//                Animation anim = AnimationUtils.loadAnimation(context, R.anim.camera_reverse);
+
+                Animation anim = new TranslateAnimation(Utils.dip2px(context, -1 * Utils.dip2px(context, width)), 0, 0,0);
+                anim.setDuration(500);
+
+
                 anim.setFillAfter(true);
                 popLayout.startAnimation(anim);
                 openStatus = false;
             } else {
-                Animation anim = AnimationUtils.loadAnimation(context, R.anim.camera);
+//                Animation anim = AnimationUtils.loadAnimation(context, R.anim.camera);
+                Animation anim = new TranslateAnimation(0, Utils.dip2px(context, -1 * Utils.dip2px(context, width)), 0,0);
+                anim.setDuration(500);
+
                 anim.setFillAfter(true);
                 popLayout.startAnimation(anim);
                 openStatus = true;
