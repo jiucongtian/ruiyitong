a6881ad0d9a712096231d9e3150b6c4c638eec76
zhenghao <jiucongtian@163.com>
2018-03-06 22:05:00 +0800

    添加云笔记编辑

--- student/src/main/java/com/example/student/pages/note/EditNoteActivity.java
+++ student/src/main/java/com/example/student/pages/note/EditNoteActivity.java
@@ -1,14 +1,17 @@
 package com.example.student.pages.note;
 
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.example.baselibrary.base.BaseActivity;
 import com.example.student.R;
 import com.mikepenz.iconics.IconicsDrawable;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by howard on 2018/3/6.
@@ -41,5 +44,30 @@ public class EditNoteActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    @OnClick({R.id.edit_btn})
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.edit_btn:
+                showEditNoteDialog();
+                break;
+        }
+
+    }
+
+    private void showEditNoteDialog() {
+        AlertDialog dlg = new AlertDialog.Builder(this, R.style.AlertDialogStyle).create();
+        dlg.show();
+        dlg.getWindow().setContentView(R.layout.dialog_edit_note);
+
+        //dlg.getWindow().findViewById(R.id.btn2).setSelected(true);
+
+
+        dlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        dlg.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
     }
 }
